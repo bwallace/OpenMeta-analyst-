880e66d81daf2d8e4f4ccb97cf80f28e463bf158
byron.wallace <byron.wallace@gmail.com>
2009-09-21 19:21:11 -0400

    more work on the binary data 2x2 entry form

---
+++ new_outcome_form.py
@@ -0,0 +1,17 @@
+
+from PyQt4.Qt import *
+
+import ui_new_outcome
+
+class AddNewOutcomeForm(QDialog, ui_new_outcome.Ui_Dialog):
+    
+    def __init__(self, parent=None):
+        super(AddNewOutcomeForm, self).__init__(parent)
+        self.setupUi(self)
+        self._populate_combo_box()
+        
+    def _populate_combo_box(self):
+        for name, id in zip([QString(s) for s in ["Binary", "Continuous", "Diagnostic", "Other"]],
+                                     [QVariant(i) for i in range(4)]):
+            self.datatype_cbo_box.addItem(name, id)
+        
