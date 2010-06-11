ea36cc6e1054a76540042fb4ae94a32f2f2d8166
byron.wallace <byron.wallace@gmail.com>
2010-06-11 08:43:01 -0400

    more work on the console

--- qconsole.py
+++ qconsole.py
@@ -1,8 +1,10 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+START_COLUMN = 3
 
 class QConsole(QTextEdit):
+        
     def __init__(self, parent):
         super(QConsole, self).__init__(parent)
         self.parent = parent
@@ -14,6 +16,12 @@ class QConsole(QTextEdit):
             self.emit(SIGNAL("upArrowPressed()"))
         elif event.key() == Qt.Key_Down:
             self.emit(SIGNAL("downArrowPressed()"))
+        elif event.key() in (Qt.Key_Left, Qt.Key_Backspace) and \
+               self.textCursor().columnNumber() == START_COLUMN:
+             # we just want to 'block' here, i.e., do nothing; the user
+             # has navigated to the start of the column
+            pass
+            
         else:
             #self.keyPressEvent(event)
             super(QConsole, self).keyPressEvent(event)
