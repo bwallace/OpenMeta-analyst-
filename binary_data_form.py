e34ca6758477c61dc183f7728a1ef8eeb9a5bf88
byron.wallace <byron.wallace@gmail.com>
2010-10-29 15:04:36 -0400

    just double checking

--- binary_data_form.py
+++ binary_data_form.py
@@ -71,11 +71,13 @@ class BinaryDataForm2(QDialog, ui_binary_data_form.Ui_BinaryDataForm):
         effect_dict = self.ma_unit.effects_dict[self.cur_effect]
         for s, txt_box in zip(['display_est', 'display_lower', 'display_upper'], \
                               [self.effect_txt_box, self.low_txt_box, self.high_txt_box]):
+            txt_box.blockSignals(True)
             if effect_dict[s] is not None:
                 txt_box.setText(QString("%s" % round(effect_dict[s], NUM_DIGITS)))
             else:
                 txt_box.setText(QString(""))
-        
+            txt_box.blockSignals(False)
+            
     def effect_changed(self):
         self.cur_effect = unicode(self.effect_cbo_box.currentText().toUtf8(), "utf-8")
         self.set_current_effect()
@@ -139,6 +141,7 @@ class BinaryDataForm2(QDialog, ui_binary_data_form.Ui_BinaryDataForm):
         self._fillin_basics(row, col) 
         params = self._get_vals()
         computed = meta_py_r.fillin_2x2(params)
+        
         print computed
         if computed is not None:
             if max(computed['residuals'].values()) > THRESHOLD:
@@ -169,17 +172,21 @@ class BinaryDataForm2(QDialog, ui_binary_data_form.Ui_BinaryDataForm):
          
     def _set_vals(self, computed_d):
         self.raw_data_table.blockSignals(True)
-        self._set_table_cell(0, 0, computed_d["c11"])
-        self._set_table_cell(0, 1, computed_d["c12"])
-        self._set_table_cell(1, 0, computed_d["c21"])
-        self._set_table_cell(1, 1, computed_d["c22"])  
-        self._set_table_cell(0, 2, computed_d["r1sum"])
-        self._set_table_cell(1, 2, computed_d["r2sum"])
-        self._set_table_cell(2, 0, computed_d["c1sum"])
-        self._set_table_cell(2, 1, computed_d["c2sum"])  
-        self._set_table_cell(2, 2, computed_d["total"])  
+        self._set_val(0, 0, computed_d["c11"])
+        self._set_val(0, 1, computed_d["c12"])
+        self._set_val(1, 0, computed_d["c21"])
+        self._set_val(1, 1, computed_d["c22"])  
+        self._set_val(0, 2, computed_d["r1sum"])
+        self._set_val(1, 2, computed_d["r2sum"])
+        self._set_val(2, 0, computed_d["c1sum"])
+        self._set_val(2, 1, computed_d["c2sum"])  
+        self._set_val(2, 2, computed_d["total"])  
         self.raw_data_table.blockSignals(False)
         
+    def _set_val(self, i, j, val):
+        is_NaN = lambda x: x != x
+        if val >= 0 and not is_NaN(val):
+            self._set_table_cell(i, j, val)
         
     def _set_table_cell(self, i, j, val):
         # try to case to an int
@@ -237,6 +244,11 @@ class BinaryDataForm2(QDialog, ui_binary_data_form.Ui_BinaryDataForm):
         return not True in [self._is_empty(row, col) for col in range(2)]
         
     def _update_data_table(self):        
+        '''
+        boring code that tries to compute/fill-in the basic 2x2 table from
+        provided information. sort of verbose, should probably make more 
+        concise.
+        '''
         self.raw_data_table.blockSignals(True)
         # now compute the numbers with no events, if possible.
         # 
@@ -245,54 +257,117 @@ class BinaryDataForm2(QDialog, ui_binary_data_form.Ui_BinaryDataForm):
         raw_data_list = []
         for group in self.cur_groups:
             raw_data_list.extend(self.raw_data_d[group])
-            
+        
         e1, n1, e2, n2 = [int(x) if (x != "" and x is not None) else None for x in raw_data_list]
         
         print "updating raw data with:\n e1 = %s, n1 = %s, e2 = %s, n2 = %s" % \
                                             (e1, n1, e2, n2)
         no_events1, no_events2 = None, None
         
-        
-        # also, instead of checking if None, check if None or "" (either way it's considered 'empty')
-        if e1 is not None and n1 is not None and self._is_empty(0, 1):
-            no_events1 = n1 - e1
-            self.raw_data_table.setItem(0, 1, \
-                                                        QTableWidgetItem(str(no_events1)))
-                                                 
-        if e2 is not None and n2 is not None and self._is_empty(1, 1):
-            no_events2 = n2 - e2
-            self.raw_data_table.setItem(1, 1, \
-                                                        QTableWidgetItem(str(no_events2)))
+        ### some basic row/column sums for the 2x2 table
+        # first, try to compute the interior of the table
+        # here is row 1
+        if e1 is not None:
+            # we've got events 1 data
             
+            # note that instead of checking if None, we check if None or "" 
+            #  (either way it's considered 'empty')
+            if n1 is not None:
+                no_events1 = n1 - e1
+                self.raw_data_table.setItem(0, 1, \
+                                                QTableWidgetItem(str(no_events1)))
+            elif not self._is_empty(0, 1):
+                # we have e1 and no-events 1; total to get n1
+                n1 = e1 + self._get_int(0, 1)
+                self.raw_data_table.setItem(0, 2, \
+                                                QTableWidgetItem(str(n1)))
+        elif n1 is not None:
+            # no events given, but we have the total
+            if not self._is_empty(0, 1):
+                e1 = n1 - self._get_int(0, 1)                                
+                self.raw_data_table.setItem(0, 0, \
+                                            QTableWidgetItem(str(e1)))
 
+        # row 2                       
+        if e2 is not None:
+            # we have events 2 data.
+            if n2 is not None:
+                no_events2 = n2 - e2
+                self.raw_data_table.setItem(1, 1, \
+                                            QTableWidgetItem(str(no_events2)))
+            elif not self._is_empty(1, 1):
+                # we have number no-events, too
+                pyqtRemoveInputHook()
+                pdb.set_trace()
+                n2 = self._get_int(1, 1) + e2
+                self.raw_data_table.setItem(1, 2, \
+                                            QTableWidgetItem(str(n2)))
+        
+                                
+        elif n2 is not None:
+            if not self._is_empty(1, 1):
+                e2 = n2 - self._get_int(1,1)
+                self.raw_data_table.setItem(1, 0, \
+                                            QTableWidgetItem(str(e2))) 
+        
         # total the totals (if possible)
+        n1 = self._get_int(0,2)
+        n2 = self._get_int(1,2)
         if n1 is not None and n2 is not None:
             self.raw_data_table.setItem(2, 2, \
-                                                        QTableWidgetItem(str(n1 + n2)))
-
+                                            QTableWidgetItem(str(n1 + n2)))
+ 
+            
+        no_events1 = self._get_int(0,1)
+        no_events2 = self._get_int(1,1)
+        
         # and the totals of *no* events
         if no_events1 is not None and no_events2 is not None:
             self.raw_data_table.setItem(2, 1, \
-                                                        QTableWidgetItem(str(no_events1 + no_events2)))
+                                            QTableWidgetItem(str(no_events1 + no_events2)))
                     
         # and now compute the sum of events
+        total_events = self._get_int(2, 0)
+        total_no_events =  self._get_int(2, 1)
+            
         if e1 is not None and e2 is not None:
             no_events_total = e1 + e2
             self.raw_data_table.setItem(2, 0, \
-                                                        QTableWidgetItem(str(no_events_total)))
-                
+                                            QTableWidgetItem(str(no_events_total)))
+
+        elif total_events is not None:
+            if e1 is not None:
+                e2 = total_events - e1
+                self.raw_data_table.setItem(1, 0, \
+                                            QTableWidgetItem(str(e2)))
+            elif e2 is not None:
+                e1 = total_events - e2
+                self.raw_data_table.setItem(1, 0, \
+                                            QTableWidgetItem(str(e1)))
+        if total_no_events is not None:
+            if no_events1 is not None:
+                no_events2 = total_no_events - no_events1
+                self.raw_data_table.setItem(1, 1, \
+                                            QTableWidgetItem(str(no_events2)))
+            elif no_events2 is not None:
+                no_events1 = total_no_events - no_events2
+                self.raw_data_table.setItem(0, 1, \
+                                            QTableWidgetItem(str(no_events1)))
+        if total_events is not None and total_no_events is not None:
+            total_total = total_events + total_no_events
+            self.raw_data_table.setItem(2, 2, \
+                                        QTableWidgetItem(str(total_total)))              
         self.raw_data_table.blockSignals(False)
+        
         
     def _is_empty(self, i, j):
         val = self.raw_data_table.item(i,j)
         return val is None or val.text() == ""
         
-        
     def _get_int(self, i, j):
         if not self._is_empty(i,j):
             int_val = int(float(self.raw_data_table.item(i, j).text()))
             return int_val
-        
             
     def try_to_update_cur_outcome(self):
         e1, n1, e2, n2 = self.ma_unit.get_raw_data_for_groups(self.cur_groups)
