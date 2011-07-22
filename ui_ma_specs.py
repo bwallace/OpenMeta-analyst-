ea1436e1cd3f306c9d8a655f1eb65d26d59bcc1e
byron.wallace <byron.wallace@gmail.com>
2011-07-22 08:42:38 -0400

    fixing specs UI

--- ui_ma_specs.py
+++ ui_ma_specs.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ma_specs2.ui'
 #
-# Created: Tue Jun 14 09:53:55 2011
-#      by: PyQt4 UI code generator 4.7.7
+# Created: Fri Jul 22 08:40:49 2011
+#      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -171,7 +171,7 @@ class Ui_Dialog(object):
         self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(Dialog)
-        self.specs_tab.setCurrentIndex(1)
+        self.specs_tab.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
