19b0f87950bc4c29d06d5b96ef9419f884430082
paul Trow <ptrow@tuftsmedicalcenter.org>
2012-02-22 16:12:16 -0500

    Changed label fixed effects to fixed effect in cov_reg_dlg2, as per Ethans recommendation.

--- ui_meta_reg.py
+++ ui_meta_reg.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'cov_reg_dlg2.ui'
 #
-# Created: Tue Dec 27 13:46:19 2011
-#      by: PyQt4 UI code generator 4.8.5
+# Created: Wed Feb 22 15:55:14 2012
+#      by: PyQt4 UI code generator 4.7.7
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -21,37 +21,31 @@ class Ui_cov_reg_dialog(object):
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Verdana"))
         cov_reg_dialog.setFont(font)
-        cov_reg_dialog.setWindowTitle(QtGui.QApplication.translate("cov_reg_dialog", "select covariates", None, QtGui.QApplication.UnicodeUTF8))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/images/meta.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         cov_reg_dialog.setWindowIcon(icon)
         self.verticalLayout = QtGui.QVBoxLayout(cov_reg_dialog)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.label = QtGui.QLabel(cov_reg_dialog)
-        self.label.setText(QtGui.QApplication.translate("cov_reg_dialog", "select covariates for regression:", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout.addWidget(self.label)
         spacerItem = QtGui.QSpacerItem(20, 10, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
         self.verticalLayout.addItem(spacerItem)
         self.cov_grp_box = QtGui.QGroupBox(cov_reg_dialog)
-        self.cov_grp_box.setTitle(QtGui.QApplication.translate("cov_reg_dialog", "available covariates", None, QtGui.QApplication.UnicodeUTF8))
         self.cov_grp_box.setObjectName(_fromUtf8("cov_grp_box"))
         self.verticalLayout.addWidget(self.cov_grp_box)
         spacerItem1 = QtGui.QSpacerItem(20, 30, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem1)
         self.groupBox = QtGui.QGroupBox(cov_reg_dialog)
         self.groupBox.setMinimumSize(QtCore.QSize(0, 50))
-        self.groupBox.setTitle(QtGui.QApplication.translate("cov_reg_dialog", "model type", None, QtGui.QApplication.UnicodeUTF8))
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.groupBox)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
         self.random_effects_radio = QtGui.QRadioButton(self.groupBox)
-        self.random_effects_radio.setText(QtGui.QApplication.translate("cov_reg_dialog", "random effects", None, QtGui.QApplication.UnicodeUTF8))
         self.random_effects_radio.setChecked(True)
         self.random_effects_radio.setObjectName(_fromUtf8("random_effects_radio"))
         self.verticalLayout_2.addWidget(self.random_effects_radio)
         self.fixed_effects_radio = QtGui.QRadioButton(self.groupBox)
-        self.fixed_effects_radio.setText(QtGui.QApplication.translate("cov_reg_dialog", "fixed effects", None, QtGui.QApplication.UnicodeUTF8))
         self.fixed_effects_radio.setObjectName(_fromUtf8("fixed_effects_radio"))
         self.verticalLayout_2.addWidget(self.fixed_effects_radio)
         self.verticalLayout.addWidget(self.groupBox)
@@ -68,6 +62,11 @@ class Ui_cov_reg_dialog(object):
         QtCore.QMetaObject.connectSlotsByName(cov_reg_dialog)
 
     def retranslateUi(self, cov_reg_dialog):
-        pass
+        cov_reg_dialog.setWindowTitle(QtGui.QApplication.translate("cov_reg_dialog", "select covariates", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("cov_reg_dialog", "select covariates for regression:", None, QtGui.QApplication.UnicodeUTF8))
+        self.cov_grp_box.setTitle(QtGui.QApplication.translate("cov_reg_dialog", "available covariates", None, QtGui.QApplication.UnicodeUTF8))
+        self.groupBox.setTitle(QtGui.QApplication.translate("cov_reg_dialog", "model type", None, QtGui.QApplication.UnicodeUTF8))
+        self.random_effects_radio.setText(QtGui.QApplication.translate("cov_reg_dialog", "random effects", None, QtGui.QApplication.UnicodeUTF8))
+        self.fixed_effects_radio.setText(QtGui.QApplication.translate("cov_reg_dialog", "fixed effect", None, QtGui.QApplication.UnicodeUTF8))
 
 import icons_rc
