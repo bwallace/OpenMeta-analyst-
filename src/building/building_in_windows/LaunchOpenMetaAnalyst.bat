87f062b3e15d430d39846e52acab155103959368
George Dietz <george_dietz@brown.edu>
2013-06-07 10:58:05 -0400

    more windows build

--- src/building/building_in_windows/LaunchOpenMetaAnalyst.bat
+++ src/building/building_in_windows/LaunchOpenMetaAnalyst.bat
@@ -1,4 +1,4 @@
-set R_HOME=R_dist/R-2.15.3/
+set R_HOME=R_dist/R-3.0.1/
 set R_USER=oma
-path = R_dist/R-2.15.3/bin/;R_dist/R-2.15.3/bin/i386;%PATH%
+path = R_dist/R-3.0.1/bin/;R_dist/R-3.0.1/bin/x64;%PATH%
 start launch.exe
