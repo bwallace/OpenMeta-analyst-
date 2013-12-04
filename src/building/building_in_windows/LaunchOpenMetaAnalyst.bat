a970c00e56eb61e09812a758026a7de388856b44
George Dietz <dietzgeorge@gmail.com>
2013-12-04 10:23:41 -0800

    build on windows 8

--- src/building/building_in_windows/LaunchOpenMetaAnalyst.bat
+++ src/building/building_in_windows/LaunchOpenMetaAnalyst.bat
@@ -1,4 +1,4 @@
-set R_HOME=R_dist/R-3.0.1/
+set R_HOME=R_dist/R-3.0.2/
 set R_USER=oma
-path = R_dist/R-3.0.1/bin/;R_dist/R-3.0.1/bin/x64;%PATH%
+path = R_dist/R-3.0.2/bin/;R_dist/R-3.0.2/bin/x64;%PATH%
 start launch.exe
--- src/make_win_build.bat
+++ src/make_win_build.bat
@@ -5,7 +5,7 @@ cd dist
 
 REM Copy R in to executable
 mkdir R_dist
-cp -rv "C:\Program Files\R\R-3.0.1" R_dist
+cp -rv "C:\Program Files\R\R-3.0.2" R_dist
 
 REM Copy over sample data
 cp -rv ../../sample_data .
@@ -17,3 +17,6 @@ cp -rv C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats .
 
 REM copy over launch file
 cp ../building/building_in_windows/LaunchOpenMetaAnalyst.bat .
+
+REM make batch file into exe and set icon, 64 bit version does'nt work on windows 8
+"C:\Program Files\Bat_To_Exe_Converter\Windows (32 bit)\Bat_To_Exe_Converter.exe" -bat LaunchOpenMetaAnalyst.bat -save LaunchOpenMetaAnalyst.exe -icon ../images/meta.ico
