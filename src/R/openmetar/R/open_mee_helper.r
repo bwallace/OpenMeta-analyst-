0c6fbe3e147f3fdabe488f693029a9411ba01afb
George Dietz <dietzgeorge@gmail.com>
2014-04-16 16:07:29 -0400

    added in support for annotating points in scatterplot with study labels for OpenMEE

--- src/R/openmetar/R/open_mee_helper.r
+++ src/R/openmetar/R/open_mee_helper.r
@@ -740,7 +740,7 @@ regenerate.exploratory.plot <- function(out.path, plot.path, plot.type, edited.p
 ############## End of Histogram code #################################
 
 ########################### Scatterplot code #################################
-make.scatterplot <- function(plot.path, data, params) {
+make.scatterplot <- function(plot.path, mydata, params) {
 	if (length(grep(".png", plot.path)) != 0){
 		png(file=plot.path, width=600, height=600)
 	}
@@ -748,9 +748,37 @@ make.scatterplot <- function(plot.path, data, params) {
 		pdf(file=plot.path) # the pdf device seems to not like setting height and width, width=600, height=600)
 	}
 	
-	myplot <- do.call(qplot, c(list(data$x, data$y, data=data), params))
+	#labels = 1:length(data$x)
+	
+	#myplot <- do.call(qplot, c(list(data$x, data$y, data=data, labels=labels), params))
 	#qplot(data$xvar, data$yvar, data=data)
-	print(myplot)
+	if ('slab' %in% names(mydata)) {
+		# rpy2 converts strvectors to Factors in dataframes by default
+		mydata$slab <- as.character(mydata$slab)
+	}
+	
+	tmp.data <<- mydata
+	
+	p <- ggplot(tmp.data, aes(x=x, y=y, label=tmp.data$slab, hjust=0, vjust=0)) + geom_point()
+	# add on parameters
+	if ('slab' %in% names(mydata)) {
+		p <- p+geom_text()
+	}
+	if ('xlab' %in% names(params)) {
+		p <- p + xlab(params$xlab)
+	}
+	if ('ylab' %in% names(params)) {
+		p <- p + ylab(params$ylab)
+    }
+	if ('xlim' %in% names(params)) {
+		p <- p + xlim(params$xlim)
+	}
+	if ('ylim' %in% names(params)) {
+		p <- p + ylim(params$ylim)
+	}
+	
+	print(p)
+	
 	graphics.off()
 }
 ######################## End of scatterplot code #############################
